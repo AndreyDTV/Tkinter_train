f7f2a4f9b32d79440c96637a84e570e51ba5247f
AndreyDTV <moonmarscos@gmail.com>
2024-02-19 09:56:50 +0300

    Calculator 2.0

--- Tk_train_1.py
+++ Tk_train_1.py
@@ -1,66 +1,96 @@
 import tkinter as tk
-from PIL import ImageTk
+from tkinter import messagebox
 
 
-def add_digit(digit):
+def add_digit(digit):  # Функция добавляет цифры в поле ввода
     value = display.get()
     if value[0] == '0' and len(value) == 1:
         value = value[1:]
+    display['state'] = tk.NORMAL
     display.delete(0, 'end')
     display.insert(0, value + digit)
+    display['state'] = tk.DISABLED
 
 
-def add_operation(operation):
+def add_operation(operation):  # Функция добавляет знак математической операции в поле ввода
     value = display.get()
     if value[-1] in '+-*/':
         value = value[:-1]
     elif '+' in value or '-' in value or '*' in value or '/' in value:
         calculate()
         value = display.get()
+    display['state'] = tk.NORMAL
     display.delete(0, 'end')
     display.insert(0, value + operation)
+    display['state'] = tk.DISABLED
 
 
-def calculate():
+def calculate():  # Функция вычисляющая значение выражения в поле ввода
     value = display.get()
     if value[-1] in '+-*/':
         value = value + value[:-1]
+    display['state'] = tk.NORMAL
     display.delete(0, 'end')
-    display.insert(0, eval(value))
+    try:
+        display.insert(0, eval(value))
+        display['state'] = tk.DISABLED
+    except (NameError, SyntaxError):
+        messagebox.showinfo('Внимание', 'Вы ввели недопустимые символы. Нужно вводить только цифры.')
+    except ZeroDivisionError:
+        messagebox.showinfo('Внимание', 'На ноль делить нельзя!')
 
-
-def clear():
-    value = display.get()
-    if value != '':
-        display.delete(0, 'end')
         display.insert(0, '0')
 
 
-def make_digit_button(digit):
+def clear():  # Функция очищает поле ввода
+    value = display.get()
+    if value != '':
+        display['state'] = tk.NORMAL
+        display.delete(0, 'end')
+        display.insert(0, '0')
+        display['state'] = tk.DISABLED
+
+
+def make_digit_button(digit):  # Создание кнопок с цифрами
     return tk.Button(text=digit, bd=5, font=('Arial', 15), command=lambda: add_digit(digit))
 
 
-def make_operation_button(operation):
+def make_operation_button(operation):  # Создание кнопок с операциями
     return tk.Button(text=operation, bd=5, font=('Arial', 20), fg='red', command=lambda: add_operation(operation))
 
 
-def make_calc_button(operation):
+def make_calc_button(operation):  # Создание кнопки вычисления ('=')
     return tk.Button(text=operation, bd=5, font=('Arial', 20), fg='red', command=calculate)
 
 
-def make_clear_button(operation):
+def make_clear_button(operation):  # Создание кнопки очистки поля ввода
     return tk.Button(text=operation, bd=5, font=('Arial', 20), fg='red', command=clear)
 
 
+def press_key(event):  # Функция использования кнопок клавиатуры для ввода
+    if event.char.isdigit():
+        add_digit(event.char)
+    if event.char in '+-*/':
+        add_operation(event.char)
+    if event.char == '"':
+        calculate()
+
+
+# Создание главного окна
 win = tk.Tk()
 win.title('Калькулятор')
 win.geometry(f'250x280+100+200')
 win['bg'] = '#ADD8E6'
 
-display = tk.Entry(win, justify=tk.RIGHT, font=('Arial', 15), width=15)
+win.bind('<Key>', press_key)
+
+#   Создание поля ввода
+display = tk.Entry(win, justify=tk.RIGHT, font=('Arial', 15, "bold"), width=15)
 display.insert(0, '0')
+display['state'] = tk.DISABLED
 display.grid(row=0, column=0, columnspan=4, stic='wens', padx=5)
 
+# Создание кнопок с цифрами
 make_digit_button('1').grid(row=1, column=0, stic="wens", padx=5, pady=5)
 make_digit_button('2').grid(row=1, column=1, stic="wens", padx=5, pady=5)
 make_digit_button('3').grid(row=1, column=2, stic="wens", padx=5, pady=5)
@@ -72,6 +102,7 @@ make_digit_button('8').grid(row=3, column=1, stic="wens", padx=5, pady=5)
 make_digit_button('9').grid(row=3, column=2, stic="wens", padx=5, pady=5)
 make_digit_button('0').grid(row=4, column=0, stic="wens", padx=5, pady=5)
 
+# Создание кнопок с операциями
 make_operation_button('+').grid(row=1, column=3, stic="wens", padx=5, pady=5)
 make_operation_button('-').grid(row=2, column=3, stic="wens", padx=5, pady=5)
 make_operation_button('/').grid(row=3, column=3, stic="wens", padx=5, pady=5)
@@ -80,6 +111,7 @@ make_operation_button('*').grid(row=4, column=3, stic="wens", padx=5, pady=5)
 make_calc_button('=').grid(row=4, column=2, stic="wens", padx=5, pady=5)
 make_clear_button('c').grid(row=4, column=1, stic="wens", padx=5, pady=5)
 
+# Установка размера колонок и столбцов
 win.grid_columnconfigure(0, minsize=60)
 win.grid_columnconfigure(1, minsize=60)
 win.grid_columnconfigure(2, minsize=60)
